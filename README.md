ad17b1228ee5089ca785c60518a66211858a52f7
Jeegar Goyani <jigo961257@gmail.com>
2022-07-21 11:08:51 +0530

    first upload globale pkg

---
+++ README.md
@@ -0,0 +1,27 @@
+# what is this?
+
+    this help's to given a shadows to every divs and images together in single page at time. show it's good to use this small fetures.
+
+# Installation
+
+`npm i jigoShadows --save`
+
+So...
+
+```
+import { jigoShadows } from "jigoShadows";
+
+jigoShadows({
+    appliedFor: "div",
+    shadow_way: "soft",
+    padding: false,
+});
+```
+
+## options
+
+jigoShadows supports 3 options,
+
+* *shadow_way* - _hard | soft_ (defaults to soft)
+* *padding* - _boolean_ (defaults to false)
+* *appliedFor* - _div | image_ (*required)
---
+++ index.js
@@ -0,0 +1,52 @@
+
+/**
+ * 
+ * @param {shadow_way} options | this for the assign what type of shadow's applyes ('soft' , 'hard')
+ * @param {padding} options | this for the showing padding to object/element (true/false)
+ * @param {appliedFor} options | this will deside that for which element does the shadows ('div', 'image')
+ * 
+ * @description The jigoShadows function for the applyed a shadow to the element shuch as div, image etx..
+ * 
+ * @author Jeegar Goyani (JigoGoyani)
+ * 
+ * @copyright jigoGoyani@2022 (Jeegar Goyani) https://www.linkedin.com/in/jeegar-goyani-b05965179/
+ * 
+ */
+function jigoShadows(options) {
+    let isDiv = false;
+    let isImg = false;
+    let images, divs;
+    if (options.appliedFor == "div") {
+        divs = document.querySelectorAll("div");
+        isDiv = true;
+        isImg = false;
+    } else if (options.appliedFor == "image") {
+        images = document.querySelectorAll("image");
+        isDiv = false;
+        isImg = true;
+    }
+    if (options.shadow_way === "hard")
+        options.shadow_way = "0px";
+    else
+        options.shadow_way = "15px";
+
+    if (isDiv) {
+        divs.forEach(div => {
+            div.style.boxShadow = `10px 10px ${options.shadow_way} 1px rgba(0,0,0,0.12)`;
+
+            if (options.padding) {
+                div.style.padding = '1em';
+            }
+        })
+    } else if (isImg) {
+        images.forEach(images => {
+            images.style.boxShadow = `10px 10px ${options.shadow_way} 1px rgba(0,0,0,0.12)`;
+
+            if (options.padding) {
+                images.style.padding = '1em';
+            }
+        })
+    }
+}
+
+modules.exports.jigoShadows = jigoShadows;
